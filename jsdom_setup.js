e7f84f55984ddbe34a87c25a81c3c24fad2c1878
Satake <satake@objectfoo.com>
2016-10-15 19:45:58 -0700

    add jsdom-global update styles

--- jsdom_setup.js
+++ jsdom_setup.js
@@ -1,19 +1,5 @@
 'use strict';
 
-import jsdom from 'jsdom';
-
-const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
-const win = doc.defaultView;
-
-// setup test environment to look like browser
-global.document = doc;
-global.window = win;
-
-Object.keys(window).forEach(key => {
-	if (!(key in global)) {
-		global[key] = window[key];
-	}
-});
-
+require('jsdom-global')();
 global.React = require('react');
 global.ReactDOM = require('react-dom');
